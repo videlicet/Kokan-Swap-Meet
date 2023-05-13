f0b9f07d00e6bfa996f3fdc4552d72f0ee00d14f
Christoph Bornemann <bornemch@gmail.com>
2023-05-13 16:25:00 +0200

    settings page updated, all fields present, changed to input fields

--- src/routes/2.1_User_Settings.tsx
+++ src/routes/2.1_User_Settings.tsx
@@ -14,11 +14,17 @@ import serverURL from '../../server_URL.ts'
 function UserSettings(): JSX.Element {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-  const [username, setUsername] = useState('sdfsf')
-  const [password, setPassword] = useState('')
-  const navigate = useNavigate()
 
   const { user, setUser } = useContext<any>(UserContext)
+  const [newInfo, setNewInfo] = useState({
+    username: user?.username,
+    email: user?.email,
+    password: user?.password,
+    first_name: user?.first_name,
+    last_name: user?.last_name
+  })
+
+  const navigate = useNavigate()
 
   const [portalContainer, setPortalContainer] = useState(
     document.getElementById('user-settings'),
@@ -30,12 +36,12 @@ function UserSettings(): JSX.Element {
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    setUsername('')
-    setPassword('')
+    // setUsername('')
+    // setPassword('')
   }
 
-  function handleChangeUsername(event: ChangeEvent<HTMLInputElement>) {
-    setUsername(event.target.value)
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewInfo({...newInfo, [`${event.target.name}`]: event.target.value})
   }
 
   async function onDelete() {
@@ -71,17 +77,76 @@ function UserSettings(): JSX.Element {
   return (
     <div id='user-settings'>
       <form>
-        <label>Username</label>
+        <label htmlFor='username'>Username</label>
         <div className='info-box'>
-          <span className='fixed-text'>{user?.username}</span>
+          <input
+            id='username'
+            name='username'
+            type='text'
+            value={newInfo.username}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              handleChange(event)
+            }
+          />
           <input type='submit' value='Change' />
         </div>
       </form>
 
       <form>
-        <label>Email</label>
+        <label htmlFor='first_name'>First Name</label>
+        <span> and </span>
+        <label htmlFor='last_name'>Last Name</label>
         <div className='info-box'>
-          <span className='fixed-text'>{user?.email}</span>
+          <input
+            id='first_name'
+            name='first_name'
+            type='text'
+            value={newInfo.first_name}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              handleChange(event)
+            }
+          />
+          <input
+            id='last_name'
+            name='last_name'
+            type='text'
+            value={newInfo.last_name}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              handleChange(event)
+            }
+          />
+          <input type='submit' value='Change' />
+        </div>
+      </form>
+
+      <form>
+        <label htmlFor='email'>Email</label>
+        <div className='info-box'>
+          <input
+            id='email'
+            name='email'
+            type='text'
+            value={newInfo.email}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              handleChange(event)
+            }
+          />
+          <input type='submit' value='Change' />
+        </div>
+      </form>
+
+      <form>
+        <label htmlFor='password'>Password</label>
+        <div className='info-box'>
+          <input
+            id='password'
+            name='password'
+            type='password'
+            value={newInfo.password}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              handleChange(event)
+            }
+          />
           <input type='submit' value='Change' />
         </div>
       </form>
@@ -98,3 +163,12 @@ function UserSettings(): JSX.Element {
 }
 
 export default UserSettings
+
+
+/* TRASH 
+  // const [username, setUsername] = useState(user?.username)
+  // const [email, setEmail] = useState(user?.email)
+  // const [password, setPassword] = useState(user?.password)
+  // const [first_name, setfirst_name] = useState(user?.first_name)
+  // const [last_name, setlast_name] = useState(user?.last_name)
+   */
