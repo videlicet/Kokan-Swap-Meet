c46d41e2d59b3af1fc4a204f44e765da34ff5b0a
Christoph Bornemann <bornemch@gmail.com>
2023-05-17 16:55:22 +0200

    navbar hover consistent on all elements

--- src/routes/1_App.tsx
+++ src/routes/1_App.tsx
@@ -5,7 +5,7 @@ import {
   createContext,
   ChangeEvent,
   FormEvent,
-  useRef
+  useRef,
 } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import '../styles/1_App.css'
@@ -90,93 +90,95 @@ function App(): JSX.Element {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <AssetContext.Provider value={{ searchTermHandle, setSearchTermHandle }}>
-      <PortalContext.Provider value={{ portal }}>
-        <div className='page-container'>
-          <NavLink to='/'>
-            <h1 id='brand'>
-              <img id='brand_icon' src={brand_icon} height='50' />
-              okan
-            </h1>
-          </NavLink>
+        <PortalContext.Provider value={{ portal }}>
+          <div className='page-container'>
+            <NavLink to='/'>
+              <h1 id='brand'>
+                <img id='brand_icon' src={brand_icon} height='50' />
+                okan
+              </h1>
+            </NavLink>
 
-          <nav id='header'>
-            <div className='navbar'>
-              <form
-                onSubmit={handleSearch}
-                className='navbar-element'
-                style={{
-                  height: '100%',
-                  width: '40%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  paddingRight: 0,
-                }}
-              >
-                <input
-                  className='search-bar'
-                  onChange={handleChangeSearchTerm}
-                  type='text'
-                  value={searchTerm}
-                  placeholder='Search assets'
-                ></input>
-                <button type='submit' className='search-submit'>
-                  <MagnifyingGlassIcon className='search-icon' />
-                  <span className="search-span">search</span>
-                </button>
-              </form>
-              {user && (
-                <div className='navbar-element'>
-                  <NavLink to='assets/new'>
-                    <PlusIcon style={{ position: 'relative', top: '0.1rem' }} />
-                    &nbsp;Asset
-                  </NavLink>
-                </div>
-              )}
-              <div id='profile-link' className='navbar-element'>
-                {!user && (
-                  <NavLink to='/login'>
-                    <PersonIcon
-                      className='login_icon'
-                      style={{ top: '0.2rem', left: '0.1rem' }}
-                    />
-                  </NavLink>
-                )}
+            <nav id='header'>
+              <div className='navbar'>
+                <form
+                  onSubmit={handleSearch}
+                  className='navbar-element'
+                  style={{
+                    height: '100%',
+                    width: '40%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    paddingRight: 0,
+                  }}
+                >
+                  <input
+                    className='search-bar'
+                    onChange={handleChangeSearchTerm}
+                    type='text'
+                    value={searchTerm}
+                    placeholder='Search assets'
+                  ></input>
+                  <button type='submit' className='search-submit'>
+                    <MagnifyingGlassIcon className='search-icon' />
+                    <span className='search-span'>search</span>
+                  </button>
+                </form>
                 {user && (
-                  <div className='profile-name'>
-                    <DropdownMenu user={user} />
+                  <div className='navbar-element hover'>
+                    <NavLink to='assets/new' style={{ color: 'inherit' }}>
+                      <PlusIcon
+                        style={{ position: 'relative', top: '0.1rem' }}
+                      />
+                      &nbsp;Asset
+                    </NavLink>
                   </div>
                 )}
+                <div id='profile-link' className='navbar-element hover'>
+                  {!user && (
+                    <NavLink to='/login'>
+                      <PersonIcon
+                        className='login_icon'
+                        style={{ top: '0.2rem', left: '0.1rem' }}
+                      />
+                    </NavLink>
+                  )}
+                  {user && (
+                    <div className='profile-name'>
+                      <DropdownMenu user={user} />
+                    </div>
+                  )}
+                </div>
               </div>
+            </nav>
+
+            <div id='main-container' ref={portal}>
+              <Outlet />
             </div>
-          </nav>
-
-          <div id='main-container' ref={portal}>
-            <Outlet />
           </div>
-        </div>
 
-        <footer id='footer'>
-          <div className='navbar'>
-            {footer.map((item) => (
+          <footer id='footer'>
+            <div className='navbar'>
+              {footer.map((item) => (
+                <div>
+                  <NavLink
+                    style={({ isActive }) => ({
+                      fontWeight: isActive ? 'bold' : '',
+                      color: isActive ? 'black' : 'grey',
+                      padding: isActive ? '0 0.8rem' : 0,
+                      borderRadius: isActive ? 15 : 0,
+                    })}
+                    to={`${item.route}`}
+                  >
+                    {item.name}
+                  </NavLink>
+                </div>
+              ))}
               <div>
-                <NavLink
-                  style={({ isActive }) => ({
-                    fontWeight: isActive ? 'bold' : '',
-                    color: isActive ? 'black' : 'grey',
-                    padding: isActive ? '0 0.8rem' : 0,
-                    borderRadius: isActive ? 15 : 0,
-                  })}
-                  to={`${item.route}`}
-                >
-                  {item.name}
-                </NavLink>
+                <span>Kokan © 2023</span>
               </div>
-            ))}
-            <div>
-              <span>Kokan © 2023</span>
             </div>
-          </div>
-        </footer>
+          </footer>
         </PortalContext.Provider>
       </AssetContext.Provider>
     </UserContext.Provider>
